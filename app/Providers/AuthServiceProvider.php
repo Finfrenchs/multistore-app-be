83eb0a93f163f7ba59297966489929b98aad95c8
M. Kelvin Madrianto F <muhammadkelvinmf@gmail.com>
2023-09-06 22:22:47 +0700

    add authorize, user role, update profile, password, list user, pagination, search

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,10 +3,20 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    //array for many feature for access user
+    public static $permission = [
+        'dashboard' => ['superadmin', 'admin'],
+        'user-index' => ['admin'],
+        'payment-update' => ['superadmin'],
+
+    ];
     /**
      * The model to policy mappings for the application.
      *
@@ -21,6 +31,20 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //handle access user for 1 feature
+        // Gate::define('dashboard', function(User $user) {
+        //     if ($user->role == 'superadmin') {
+        //         return true;
+        //     }
+        // });
+
+        //handle access user for many feature
+        foreach (self::$permission as $feature => $roles) {
+            Gate::define($feature, function(User $user) use ($roles) {
+                if (in_array($user->role, $roles)) {
+                    return true;
+                }
+            });
+        }
     }
 }

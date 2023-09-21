cdde7ffe99f6c112cb24d4402398c1d31dc3c3b4
M. Kelvin Madrianto F <muhammadkelvinmf@gmail.com>
2023-09-21 13:27:47 +0700

    upload

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UploadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,7 +26,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])
     ->middleware('auth:sanctum'); //supaya logout dapat di akses setelah login tambahkan midlleware
-
+Route::post('image/upload', [UploadController::class, 'uploadImage']);
+Route::post('image/upload-multiple', [UploadController::class, 'uploadMultipleImage']);
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
